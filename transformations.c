9c8e0ee40f6b5f5dfe0cf2d8c49b86167211cb84
eld3 <eld3@pc2-033-l.cs.st-andrews.ac.uk>
2014-04-05 18:20:34 +0100

    SAT->3SAT works returns new instance.

--- transformations.c
+++ transformations.c
@@ -6,6 +6,7 @@
 satinstance transform_to_3sat ( satinstance s){
 
 	//pseudo code method
+	satinstance new_s = empty_satinstance(); 
 
 	unsigned int total_clauses = number_clauses(s);
 	printf("%i\n",total_clauses );
@@ -26,8 +27,12 @@ satinstance transform_to_3sat ( satinstance s){
 				//add a new literal for every missing one
 				literal l = get_literal(curr_sat_clause,j);
 				add_literal(curr_sat_clause, l);
+				add_clause(new_s,curr_sat_clause);
 			}
 		//handle clauses that have more than 3 literals
+		}else if(clause_length == 3){
+			add_clause(new_s,curr_sat_clause);
+
 		}else if (clause_length > 3){
 			printf("%s\n","bigger than 3!" );
 			unsigned int no_of_clauses = clause_length - 2;
@@ -64,7 +69,7 @@ satinstance transform_to_3sat ( satinstance s){
 					literal lit_to_add = positive(clause_joiner);
 					add_literal(curr_clause,lit_to_add);
 					//add the whole clause to the sat instance
-					add_clause(s,curr_clause);
+					add_clause(new_s,curr_clause);
 
 				
 				}else if (curr_clause_number == no_of_clauses - 1){
@@ -81,7 +86,7 @@ satinstance transform_to_3sat ( satinstance s){
 					current_sat_literal++;
 					add_literal(curr_clause, lit_to_add);
 					//add the whole clause to the sat instance
-					add_clause(s,curr_clause);
+					add_clause(new_s,curr_clause);
 					//remove starting clause from sat instance
 
 				
@@ -99,12 +104,12 @@ satinstance transform_to_3sat ( satinstance s){
 					lit_to_add = positive(clause_joiner);
 					add_literal(curr_clause,lit_to_add);
 					//add the whole clause to the sat instance
-					add_clause(s,curr_clause);
+					add_clause(new_s,curr_clause);
 				}	
 			}			
 		}
 	}
-	return s;
+	return new_s;
 }
 
 
@@ -113,94 +118,3 @@ satinstance transform_to_3sat ( satinstance s){
 
  // }
 
-
-	//if clause length == 3
-	//move on to next clause
-
-	//if clause length < 3
-	//find n = number of literals(clause)
-	//for 3-n
-		//copy literal 1 and add to clause
-	//move on to next clause
-
-	//if clause length > 3
-	//get number_lits(clause)
-	//n = clause lenght - 2
-	//create n new clauses
-
-	//for clause 1 add first two existing literals
-	//add new literal
-	//add clause 1 to sat instance
-	//add new literals negation to clause 2
-	
-	//startloop:
-
-		//if curr literal is the  (clause length-1)th
-			//add last two literals to current clause
-			//add clause to sat instance
-			//remove starting clause from sat instance
-			//## end loop ##
-
-		//else 
-			//add curr literal to curr clause
-			//add new literal to curr clause
-			//add clause to sat instance
-			//add its negation to the next clause
-
-	//repeat loop
-
-	//loop end:
-
-	//move on to next clause
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//current panic plan
-
-//length || New Clauses || Literals ||  l-3  ||
-//   4   ||     1       ||    1     ||   1   ||
-//   5   ||     2       ||    2     ||   2   ||
-//   6   ||     3       ||    3     ||   3   ||
-
-
-//if == 4
-//create two new clauses
-//add first two literals to the first clause
-//add new literal to first clause
-//add its negation to the second clause
-//add last to literals to the second clause
-//move on
-
-//if == 5
-//create two new clauses
-//move 3rd to first new clause
-//move 4th and 5th to second new clause
-//add a new literal to clause 1 and its negation to clause 2
-//add a new literal to clause 2 and its negation to clause 3
-//move on
-
-//if == 6
-//create 3 new clauses
-//move 3rd to clause 2
-//move 4th to clause 3
-//move 5th and 6th to clause 4
-//add a new literal to clause 1 and its negation to clause 2
-//add a new literal to clause 2 and its negation to clause 3
-//add a new literal to clause 3 and its negation to clause 4
-//move on
-
-//if == 10
-//*****BEGIN HERE******//
