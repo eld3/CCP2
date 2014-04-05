edb10583b0afb216467beb78a3fc8f50f72daa41
eld3 <eld3@pc2-033-l.cs.st-andrews.ac.uk>
2014-04-05 18:06:56 +0100

    SAT->3SAT working but cannot remove old clause will have to create new SAT instance instead, or work on existing clause

--- transformations.c
+++ transformations.c
@@ -32,64 +32,78 @@ satinstance transform_to_3sat ( satinstance s){
 			printf("%s\n","bigger than 3!" );
 			unsigned int no_of_clauses = clause_length - 2;
 			printf("This will take %i clauses\n", no_of_clauses );
+
+			clause prev_clause;
+			clause curr_clause;
+			variable largest;
+			variable clause_joiner;
+			int current_sat_literal = 0;
 			
 			unsigned int curr_clause_number;
 			for (curr_clause_number = 0; curr_clause_number < no_of_clauses; curr_clause_number++){
 
-				clause curr_clause = empty_clause();
+				prev_clause = curr_clause;
+				curr_clause = empty_clause();
+				largest = largest_variable_satinstance(s);
 
 				//identify which clause is being created{}
 				if (curr_clause_number == 0){
+
 					//first clause
 					printf("%s\n","First Clause");
+					//for clause 1 add first two existing literals
+					literal first = get_literal(curr_sat_clause,current_sat_literal);
+					current_sat_literal++;
+					add_literal(curr_clause,first);					
+					literal second = get_literal(curr_sat_clause,current_sat_literal);
+					current_sat_literal++;
+					add_literal(curr_clause,second);
+					//create a variable bigger than current largest
+					clause_joiner = largest + 1;
+					//add its positive version to the clause
+					literal lit_to_add = positive(clause_joiner);
+					add_literal(curr_clause,lit_to_add);
+					//add the whole clause to the sat instance
+					add_clause(s,curr_clause);
+
 				
 				}else if (curr_clause_number == no_of_clauses - 1){
 					//last clause
 					printf("%s\n","Last Clause");
+					//add negation from previously added new literal to curr clause
+					literal lit_to_add = negative(clause_joiner);
+					add_literal(curr_clause,lit_to_add);
+					//add last two literals to current clause
+					lit_to_add = get_literal(curr_sat_clause,current_sat_literal);
+					current_sat_literal++;
+					add_literal(curr_clause, lit_to_add);
+					lit_to_add = get_literal(curr_sat_clause,current_sat_literal);
+					current_sat_literal++;
+					add_literal(curr_clause, lit_to_add);
+					//add the whole clause to the sat instance
+					add_clause(s,curr_clause);
+					//remove starting clause from sat instance
+
 				
 				}else{
 					//middle clauses
 					printf("%s\n","Middle Clause" );
-				}
-
-				
-			}
-			
-			//create new clause
-			//for clause 1 add first two existing literals
-			//add new literal
-			//add clause 1 to sat instance
-			//add new literals negation to clause 2
-	
-			//startloop:
-
-		//if curr literal is the  (clause length-1)th
-			//add last two literals to current clause
-			//add clause to sat instance
-			//remove starting clause from sat instance
-			//## end loop ##
-
-		//else 
-			//add curr literal to curr clause
-			//add new literal to curr clause
-			//add clause to sat instance
-			//add its negation to the next clause
-
-	//repeat loop
-
-	//loop end:
-
-	//move on to next clause
+					//add negation from previously added new literal to curr clause
+					literal lit_to_add = negative(clause_joiner);
+					add_literal(curr_clause,lit_to_add);
+					lit_to_add = get_literal(curr_sat_clause,current_sat_literal);
+					current_sat_literal++;
+					add_literal(curr_clause, lit_to_add);
+					clause_joiner = largest + 1;
+					//add its positive version to the clause
+					lit_to_add = positive(clause_joiner);
+					add_literal(curr_clause,lit_to_add);
+					//add the whole clause to the sat instance
+					add_clause(s,curr_clause);
+				}	
+			}			
 		}
 	}
-	
-
-	
-	
-
-	//if clause length > 3
-	
-
 	return s;
 }
 
